7aaeab3a81d15d65af6b2ff0ade243e7e7cd298b
Tasia Donnelly <1733713379@qq.com>
2025-01-04 08:53:06 +0800

    更新 custom.sh

--- SCRIPTS/custom.sh
+++ SCRIPTS/custom.sh
@@ -10,3 +10,4 @@ git clone https://github.com/herozmy/luci-app-heroproxy package/new/luci-app-her
 mkdir -p files/etc/heroproxy/ui
 git clone https://github.com/Zephyruso/zashboard.git -b gh-pages files/etc/heroproxy/ui
 
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
